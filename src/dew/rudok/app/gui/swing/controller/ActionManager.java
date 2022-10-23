b71699e9b4669396ef9c0335d1def7a355d64620
anikolic <anikolic11522@raf.rs>
2022-10-23 17:21:57 +0200

    Rad na klasi ExitAction i ActionManager.

--- src/dew/rudok/app/gui/swing/controller/ActionManager.java
+++ src/dew/rudok/app/gui/swing/controller/ActionManager.java
@@ -1,4 +1,32 @@
 package dew.rudok.app.gui.swing.controller;
 
 public class ActionManager {
+
+    private ExitAction exitAction;
+    private NewProjectAction newProjectAction;
+
+    public ActionManager(){
+        initialiseActions();
+    }
+
+    private void initialiseActions(){
+        exitAction = new ExitAction();
+        newProjectAction = new NewProjectAction();
+    }
+
+    public ExitAction getExitAction() {
+        return exitAction;
+    }
+
+    public NewProjectAction getNewProjectAction() {
+        return newProjectAction;
+    }
+
+    public void setExitAction(ExitAction exitAction) {
+        this.exitAction = exitAction;
+    }
+
+    public void setNewProjectAction(NewProjectAction newProjectAction) {
+        this.newProjectAction = newProjectAction;
+    }
 }
