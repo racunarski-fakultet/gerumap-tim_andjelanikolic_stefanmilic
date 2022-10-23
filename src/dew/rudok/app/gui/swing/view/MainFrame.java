2727014240a9c1ad258ccb2cbffca6da77297d9e
anikolic <anikolic11522@raf.rs>
2022-10-23 17:34:26 +0200

    Doslo je do greske pokusavamo da ispravimo

--- src/dew/rudok/app/gui/swing/view/MainFrame.java
+++ src/dew/rudok/app/gui/swing/view/MainFrame.java
@@ -1,10 +1,15 @@
 package dew.rudok.app.gui.swing.view;
 
+import dew.rudok.app.gui.swing.controller.ActionManager;
+
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
 
     private static MainFrame instance = null;
+    private ActionManager actionManager;
+    private JMenuBar menu;
+    private JToolBar toolBar;
 
     private  MainFrame () {
 
