5bcc25d1833133a46130706ef3480e502f08f1cf
Stefan <mstefan558@gmail.com>
2022-11-16 22:30:44 +0100

    FileLogger da Andjela proveri dal radi

--- src/main/java/dew/rudok/app/gui/swing/error/FileLogger.java
+++ src/main/java/dew/rudok/app/gui/swing/error/FileLogger.java
@@ -4,15 +4,27 @@ import dew.rudok.app.core.Logger;
 import dew.rudok.app.gui.swing.message.Message;
 import lombok.NoArgsConstructor;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 @NoArgsConstructor
 public class FileLogger implements Logger {
 
-    File f;
+    File file = new File("/resources");
 
     @Override
     public void log(Message message) {
+
+        String str = message.toString();
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(file));
+            writer.write(str);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
