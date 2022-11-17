d96bc9459486c057a1a26ca448988b953614a66f
Milki19 <mstefan558@gmail.com>
2022-11-16 21:14:49 -0800

    Ispravlanje greksi u Errorima

--- src/main/java/dew/rudok/app/gui/swing/SwingGui.java
+++ src/main/java/dew/rudok/app/gui/swing/SwingGui.java
@@ -1,7 +1,10 @@
 package dew.rudok.app.gui.swing;
 
 import dew.rudok.app.core.Gui;
+import dew.rudok.app.gui.swing.message.Message;
 import dew.rudok.app.gui.swing.view.MainFrame;
+
+import javax.swing.*;
 
 public class SwingGui implements Gui {
 
@@ -15,6 +18,6 @@ public class SwingGui implements Gui {
 
     @Override
     public void update(Object object) {
-
+        JOptionPane.showInputDialog(MainFrame.getInstance(), ((Message)object).getContent());
     }
 }
